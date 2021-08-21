85fd45cf7aa3c1e18b03eb82b77caa3b9550cf84
IlyasDevelopment <Ilyas2707@yandex.ru>
2021-08-21 21:55:55 +0300

    viewsets and routers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
 app_name = 'courses'
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 urlpatterns = [
     path('subjects/',
@@ -13,7 +17,5 @@ urlpatterns = [
          views.SubjectDetailView.as_view(),
          name='subject_detail'),
 
-    path('courses/<pk>/enroll/',
-         views.CourseEnrollView.as_view(),
-         name='course_enroll'),
+    path('', include(router.urls)),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -7,6 +7,9 @@ from rest_framework.response import Response
 from ..models import Course
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+from rest_framework.decorators import action
 
 
 class SubjectListView(generics.ListAPIView):
@@ -19,11 +22,15 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
